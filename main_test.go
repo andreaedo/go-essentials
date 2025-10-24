7f04e51af418486d29fbdc1b7fe19b7d7fa17b04
andreaedo <andreaedo.desanto@gmail.com>
2025-10-24 16:00:26 +0200

    tests and assertions

--- main_test.go
+++ main_test.go
@@ -7,11 +7,25 @@ import (
 func TestTruck(t *testing.T) {
 	t.Run("processTruck", func(t *testing.T) {
 		t.Run("should load and unload a truck cargo", func(t *testing.T) {
+			nt := &NormalTruck{id: "1", cargo: 42}
+			et := &ElectricTruck{id: "2"}
 
-			//err = processTruck(et)
-			//if err != nil {
-			//	t.Fatalf("error processing truck: %s", err)
-			//}
+			if err := processTruck(nt); err != nil {
+				t.Fatalf("Error processing truck: %s\n", err)
+			}
+
+			if err := processTruck(et); err != nil {
+				t.Fatalf("Error processing truck: %s\n", err)
+			}
+
+			//asserting
+			if nt.cargo != 0 {
+				t.Fatal("normal truck cargo should be 0")
+			}
+
+			if et.battery != -2 {
+				t.Fatal("electric truck battery should be -2")
+			}
 
 		})
 	})
